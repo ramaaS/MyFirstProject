2ed7fab066044ab6993e455f145b6d9e8e628dcc
Ashok <ashokgoud1785@gmail.com>
2017-07-26 11:40:01 -0700

    removing the multiple console.writelines

--- Addition/Addition/Program.cs
+++ Addition/Addition/Program.cs
@@ -10,13 +10,11 @@ namespace Addition
     {
         static void Main(string[] args)
         {
-            int a = 10, b = 20, c;
+            int a = 10, b = 20, c,d,e;
             c = a + b;
-            Console.WriteLine(c);
-            c = a - b;
-            Console.WriteLine(c);
-            c = a / b;
-            Console.WriteLine(c);
+            d = a - b;
+            e = a / b;
+            Console.WriteLine("{0} {1} {2}",c,d,e);
             Console.ReadKey();
             
         }
