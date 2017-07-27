048f057836d290c9c7424679326e30c9406e1fb5
Ashok <ashokgoud1785@gmail.com>
2017-07-26 19:19:47 -0700

    adding multiple feature

--- Addition/Addition/Program.cs
+++ Addition/Addition/Program.cs
@@ -10,11 +10,12 @@ namespace Addition
     {
         static void Main(string[] args)
         {
-            int a = 10, b = 20, c,d,e;
+            int a = 10, b = 20, c,d,e,f;
             c = a + b;
             d = a - b;
             e = a / b;
-            Console.WriteLine("{0} {1} {2}",c,d,e);
+            f = a * b;
+            Console.WriteLine("{0} {1} {2}",c,d,e,f);
             Console.ReadKey();
             
         }
